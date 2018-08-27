7607a22f2b54b0b775d691be2d2571f98306d22f
System Administrator <root@KasodeMacBook-Pro.local>
2018-08-27 19:03:51 +0800

    完成玩功能登录

--- src/service/base.js
+++ src/service/base.js
@@ -1,18 +1,18 @@
-import wepy from 'wepy'
+// import wepy from 'wepy'
 
-export default auth;
+// export default auth;
 
-class auth {
-  constructor() {
-    this.name = "kaso"
-  }
-  getOpenId() {
-    let userCode = wepy.login()
-    console.log(userCode)
-  }
-}
+// class auth {
+//   constructor() {
+//     this.name = "kaso"
+//   }
+//   getOpenId() {
+//     let userCode = wepy.login()
+//     console.log(userCode)
+//   }
+// }
 
-let a = new auth()
-console.log(a.name)
-console.log('ddd')
-a.getOpenId()
+// let a = new auth()
+// console.log(a.name)
+// console.log('ddd')
+// a.getOpenId()
